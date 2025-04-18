51f1464557f3ac7e5a1dcb7ccc40f1e580c4e47e
Sedi <sedi@gmail.com>
2025-04-18 14:26:40 +0330

    Footer commit

--- car-rent/components/Footer.tsx
+++ car-rent/components/Footer.tsx
@@ -1,5 +1,8 @@
+import { footerLinks } from '@/constants'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+
 
 const Footer = () => {
   return (
@@ -8,15 +11,34 @@ const Footer = () => {
         <div className='flex flex-col justify-center items-start gap-6'>
           <Image src="/logo.svg" alt="Logo footer" width={118} height={18} className='object-contain' />
           <p className='text-base text-gray-700'>
-            Carhub 2023 <br />
-          All Rights Reserved &copy;
+             Carhub 2023 <br />
+             All Rights Reserved &copy;
           </p>
         </div>
+
         <div className='footer__links' >
+          {footerLinks.map((link) => (
+            <div key={link.title}
+            className='footer__link'
+            >
+              <h3 className='font-bold' >{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  key={item.title}
+                  href={item.url}     
+                  className='text-gray-500'
+                >
+                  {item.title}
+                </Link>
+                
+              ))}
+              
+           </div>
+         ))}
           
         </div>
 
-      </div>
+  </div>
       
 
    </footer>
