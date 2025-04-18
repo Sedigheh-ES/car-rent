5e438661639dfe61f226f0dba666c4f269115e95
Sedi <sedi@gmail.com>
2025-04-18 09:54:59 +0330

    Button-2

--- car-rent/components/Hero.tsx
+++ car-rent/components/Hero.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import React from 'react'
 import CustomButton from './CustomButton'
+import Image from 'next/image'
 
 const Hero = () => {
+  const handleScroll= () => {
+    console.log("button clicked");
+  }
   return (
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
@@ -12,7 +18,12 @@ const Hero = () => {
            Streamline your car rental experience with our effortless booking
           process.
         </p>
-        <CustomButton/>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick = {handleScroll}
+        
+        />
 
       </div>
 
