5cda8c8d1e2d49007dab88691fb5d8da471e3ee9
Sedi <sedi@gmail.com>
2025-04-18 13:44:36 +0330

    last change

--- car-rent/components/index.ts
+++ car-rent/components/index.ts
@@ -1,18 +1,11 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
-<<<<<<< HEAD
-
-export {
-    Hero,
-    CustomButton
-=======
-import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 export {
     Hero,
     CustomButton,
     Navbar,
     Footer
->>>>>>> 8fdc145 (Last change)
 }
