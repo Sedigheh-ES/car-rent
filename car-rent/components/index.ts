bb647a61e2696cf023f223f9d17b26d7b7689387
Sedi <sedi@gmail.com>
2025-04-18 17:50:47 +0330

    search bar app

--- car-rent/components/index.ts
+++ car-rent/components/index.ts
@@ -2,10 +2,14 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import SearchBar from "./searchBar";
+import CustomFilter from "./CustomFilter";
 
 export {
     Hero,
     CustomButton,
     Navbar,
+    SearchBar,
+    CustomFilter,
     Footer
 }
