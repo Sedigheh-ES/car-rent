a0d4aa352e8cabf7fe849db4479857f6bf21eb43
Sedi <sedi@gmail.com>
2025-04-19 11:21:14 +0330

    Api Coonection set

---
+++ car-rent/utils/index.ts
@@ -0,0 +1,13 @@
+export async function fetchCars() {
+    const headers = {
+        'x-rapidapi-key': 'fc1584a21dmsh925737ce34dd977p146504jsn8bfc3b7a5db3',
+		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars/', {
+        headers: headers,
+    });
+
+    const result = await response.json();
+    return result;
+}
